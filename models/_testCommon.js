2a9db8d90926211fcaa3e235f88d4b9d4ed88cb0
brendan <lobrendan@gmail.com>
2022-02-25 12:05:33 -0800

    updated test common file

--- models/_testCommon.js
+++ models/_testCommon.js
@@ -8,6 +8,22 @@ async function commonBeforeAll() {
   await db.query("DELETE FROM companies");
   // noinspection SqlWithoutWhere
   await db.query("DELETE FROM users");
+  // noinspection SqlWithoutWhere
+  await db.query("DELETE FROM jobs");
+
+  await db.query(`
+    INSERT INTO users(username,
+                      password,
+                      first_name,
+                      last_name,
+                      email)
+    VALUES ('u1', $1, 'U1F', 'U1L', 'u1@email.com'),
+            ('u2', $2, 'U2F', 'U2L', 'u2@email.com')
+    RETURNING username`,
+    [
+      await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
+      await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
+    ]);
 
   await db.query(`
     INSERT INTO companies(handle, name, num_employees, description, logo_url)
@@ -16,18 +32,10 @@ async function commonBeforeAll() {
            ('c3', 'C3', 3, 'Desc3', 'http://c3.img')`);
 
   await db.query(`
-        INSERT INTO users(username,
-                          password,
-                          first_name,
-                          last_name,
-                          email)
-        VALUES ('u1', $1, 'U1F', 'U1L', 'u1@email.com'),
-               ('u2', $2, 'U2F', 'U2L', 'u2@email.com')
-        RETURNING username`,
-      [
-        await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
-        await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
-      ]);
+    INSERT INTO jobs(title, salary, equity, company_handle)
+    VALUES ('j1', 1000, .01, 'c1'),
+            ('j2', 2000, .02, 'c2'),
+            ('j3', 3000, .03, 'c3')`);
 }
 
 async function commonBeforeEach() {
